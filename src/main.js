95ddd58e4b31e021a3c192a33a3a41b131d93d6a
bmonagan <bernardmonagan@gmail.com>
2026-03-03 17:17:12 -0500

    fix: correct error throwing in Book constructor and add id property

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
     if (!new.target){
-        throw error("Book must be called with new");
+        throw new Error("Book must be called with new");
     }   
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = crypto.randomUUID();
     this.info = function(){
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
     }
 }
+
+function addBookToLibrary() {
+    let title = prompt("Title:");
+    let author = prompt("Author:");
+    let pages = prompt("Pages:");
+    let read = prompt("Read:");
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+addBookToLibrary();
